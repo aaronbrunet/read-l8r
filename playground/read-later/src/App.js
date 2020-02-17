ac46a77a9d5b7b702f5d7d91fa31c8ec3e015cba
aaronbrunet <awbrunet@gmail.com>
2020-02-17 18:26:45 -0500

    Implemented basic add to state

--- playground/read-later/src/App.js
+++ playground/read-later/src/App.js
@@ -2,12 +2,26 @@ import React from 'react'
 import List from './components/list.js'
 
 const App = () => {
+
+  const links = [
+    {
+      url: "google.com",
+      description: "Google",
+      read: false
+    },
+    {
+      url: "example.com",
+      description: "Example",
+      read: true
+    },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <p>Hello World!</p>
-      <List />
+      <List data={links} />
     </div>
   );
 }
--- playground/read-later/src/components/list.js
+++ playground/read-later/src/components/list.js
@@ -1,21 +1,44 @@
 import React, {useState} from 'react'
 
 const List = (props) => {
-    const [newLink,addNewLink] = useState('');
+    const [link,addLink] = useState(props.data);
+    const list = props.data;
+    const add = (url,description,read) => {
+        //const {url,description,read} = url,description,read
+        //addNewLink({[event.target.name]:event.target.innerHTML})
+        addLink([...list, {url:url, description:description,read:read}])
+        //addLink({url:url, description:description,read:read})
+    }
+
+    const rows = list.map((row,index) => {
+        return (
+            <tr key={index}>
+                <td>{row.url}</td>
+                <td>{row.description}</td>
+                <td>{row.read.toString()}</td>
+                <td onClick={()=>add(row.url,row.description,row.read)}>Add This Link</td>
+            </tr>
+        )
+    })
+
     return(
         <table>
             <thead>
                 <tr>
                     <th>URL</th>
                     <th>Description</th>
+                    <th>Read?</th>
                     <th>State</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>{newLink}</td>
-                    <td onClick={()=>addNewLink('Clicked!')}>Add New Link</td>
-                </tr>
+                {rows}
+                <tr key='3'>
+                <td>Test.com</td>
+                <td>Test</td>
+                <td>false</td>
+                <td onClick={()=>add("Test.com","Test",false)}>Add This Link</td>
+            </tr>
             </tbody>
         </table>
     );
