0566e8577122f344224cbfa4a4663e89e7bf2258
aaronbrunet <awbrunet@gmail.com>
2020-02-17 11:24:55 -0500

    moving static to master root

---
+++ playground/tip/precache-manifest.817579708494f81f7ab891f95fe197a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf1513014f84ba0496112212752ecae",
+    "url": "/playground/tip/index.html"
+  },
+  {
+    "revision": "6e7885aa0c9a07e09bdc",
+    "url": "/playground/tip/static/css/main.7cac2a67.chunk.css"
+  },
+  {
+    "revision": "7a4ae0658a972978a4cd",
+    "url": "/playground/tip/static/js/2.3dbf20c0.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/playground/tip/static/js/2.3dbf20c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7885aa0c9a07e09bdc",
+    "url": "/playground/tip/static/js/main.cf471a94.chunk.js"
+  },
+  {
+    "revision": "00b2bdf247f9b3aea732",
+    "url": "/playground/tip/static/js/runtime-main.238b97df.js"
+  },
+  {
+    "revision": "e2e490fb6d6ccaa0a823290567b61348",
+    "url": "/playground/tip/static/media/bill-bg.e2e490fb.svg"
+  },
+  {
+    "revision": "62b598fc6b4c90011c535f74cd2115e1",
+    "url": "/playground/tip/static/media/bottom-bg-2.62b598fc.svg"
+  },
+  {
+    "revision": "c76d1c6a863870fdd67329e5788cfecb",
+    "url": "/playground/tip/static/media/controls-bg.c76d1c6a.svg"
+  },
+  {
+    "revision": "178b9dd517435c5385cca84e115a6db9",
+    "url": "/playground/tip/static/media/top-bg.178b9dd5.svg"
+  }
+]);
