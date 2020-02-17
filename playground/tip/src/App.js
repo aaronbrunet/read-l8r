d2eaf412850f2bd07ce4e0b38a6ec408f6c515ed
aaronbrunet <awbrunet@gmail.com>
2020-02-16 23:09:19 -0500

    rebasing

--- playground/tip/src/App.js
+++ playground/tip/src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 
-import Entry from './entry.js';
-import Tip from './tip.js';
-import Bill from './bill.js';
-//import logo from './logo.svg';
-import './App.css';
-import './main.css'
+import Entry from './components/entry.js';
+import Tip from './components/tip.js';
+import Bill from './components/bill.js';
+//import './styles/App.css';
+import './styles/main.css'
+import './styles/styles.scss'
+
 
 const tips = [10,15,18,20];
 
@@ -14,65 +15,117 @@ class App extends Component {
     super();
     this.state = {
       bill: '0.00',
+      bill_tip:'0.00',
       share: '0.00',
       tip: '0.00',
-      percent: 0
+      tip_share: '0.00',
+      count: 1,
+      percent: 0,
+      roundUp: false
     }
   }
 
-  eval = event => {    
-    const { value } = event.target
-    let tip = parseFloat(this.state.tip);
-    let share = parseFloat(this.state.share);
-    let bill = parseFloat(value);
-    tip = bill*parseFloat(this.state.percent);
-    share = parseFloat(value) + tip;
-    this.setState({ bill: bill.toFixed(2), share: share.toFixed(2), tip: tip.toFixed(2)})    
+  headcount = (bill,count) =>{
+    this.setState({count:count})
+    this.eval(bill,count,null);
+  }
+  
+  rounding = (round) => {
+    this.setState({roundUp:round})  
+    this.eval(null,null,round);
   }
 
+  roundUp(bill_tip){
+      let val=bill_tip;
+      val = Math.ceil(val);
+      return val;
+  }
+
+  //calc by input value
+  eval = (bill_,count_,round_) => {    
+    let bill,count,round;
+    bill_ !== null ? bill = bill_ : bill = this.state.bill;
+    count_ !== null ? count = count_ : count = this.state.count;    
+    round_ !== null ? round = round_ : round = this.state.roundUp;    
+    if(bill>0){
+      bill = parseFloat(bill);
+      let tip = parseFloat(this.state.tip);
+      let bill_tip = parseFloat(this.state.bill_tip);
+      try { 
+        tip = bill*parseFloat(this.state.percent);
+        bill_tip = bill + tip;
+        if(round){
+          bill_tip = this.roundUp(bill_tip);
+          tip = bill_tip - bill;
+        }
+        let share = bill_tip/count;    
+        let tip_share = tip/count;
+        this.setState({ bill: bill.toFixed(2), bill_tip: bill_tip.toFixed(2), tip: tip.toFixed(2), tip_share: tip_share.toFixed(2),share: share.toFixed(2)})    
+      }catch (e){
+        this.setState({
+        bill_tip:"error"
+      })
+      }
+    }
+  }
+
+  //calc by percent
   calculate = (value) =>{    
+    let percent;
+    value>0 ? percent = parseFloat(value) : percent = 0
     let bill = parseFloat(this.state.bill);
     let tip = parseFloat(this.state.tip);
-    let share = parseFloat(this.state.share);//this.state.share;
-    let percent = parseFloat(value);    
+    let bill_tip = parseFloat(this.state.bill);//this.state.bill_tip;      
+    let share = parseFloat(this.state.share);
     try {      
       tip = bill*percent;
-      share = bill + tip;
+      bill_tip = bill + tip;
+      if(this.state.roundUp){
+        bill_tip = this.roundUp(bill_tip);
+        tip = bill_tip - bill;
+      } 
+      share = bill_tip / this.state.count;
+      let tip_share = tip/this.state.count;
       this.setState({
-        share: share.toFixed(2).toString(), tip: tip.toFixed(2).toString(), percent: percent})
+        bill_tip: bill_tip.toFixed(2).toString(), tip: tip.toFixed(2).toString(), tip_share: tip_share.toFixed(2), share: share.toFixed(2), percent: percent})
     }catch (e){
       this.setState({
-        share:"error"
+         bill_tip:"error"
       })
-    }
+    }    
   };  
 
   reset = () =>{
-    let share = this.state.share;
-    let bill = this.state.bill;
-    let tip = this.state.tip;
-    try {
-      share = 0;
-      bill = 0;
-      tip = 0;
-      this.setState({share: share.toFixed(2), bill:bill.toFixed(2), tip:tip.toFixed(2)})
-    }catch (e){
-      this.setState({
-        share:"error"
-      })
-    }
+    this.setState({
+      bill: '0.00',
+      bill_tip:'0.00',
+      share: '0.00',
+      tip: '0.00',
+      tip_share: '0.00',
+      percent: 0
+    });
   };  
 
   render(){ 
     return (
-      <div className="App">
-        <h1>Just The Tip!</h1>
-        <div className='container small-container'>
-          <Entry eval={this.eval} reset={this.reset}/> 
-          <Tip tips={tips} click={this.calculate} />
-          <Bill bill={this.state.bill} share={this.state.share} tip={this.state.tip}/>
+      <>
+      <div className="main">
+        <div className="title">
+          <h1>A Friendly Tip</h1>
+          <p>Tired of fighting with friends over who pays what, and how much tip to leave?
+            <br/><br/>Fight no longer!
+            <br/>…about that.</p>
+        </div>
+        <div className='container'>
+          <div className='controls'>
+          <Entry eval={this.eval} count={this.state.count} reset={this.reset} /> 
+          <Tip tips={tips} calculate={this.calculate} reset={this.reset} />
+          </div>
+          <Bill headcount={this.headcount} count={this.state.count} bill={this.state.bill} bill_tip={this.state.bill_tip} tip={this.state.tip} tip_share={this.state.tip_share} share={this.state.share} rounding={this.rounding}/>
         </div>
       </div>
+      <div className="bottom"><p>Created by Aaron Brunet 2020</p></div>
+      </>
     );
   }
 }
---
+++ playground/tip/src/components/bill.js
@@ -0,0 +1,59 @@
+//TODO: custom tip
+//TODO: dynamic show/hide total/each
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+    opacity: ${props=>(props.value==='0.00' ? '.5' : '1')};    
+`
+
+const Toggle = styled.span`
+    background-color: ${props=>(props.toggle ? '#F34040' : '#FFBB36')};
+`
+
+const Bill = (props) => {
+    const [toggle,setToggle] = useState(true)
+    const [roundUp,setRoundUp] = useState(false)    
+    const [count,setCount] = useState(1)
+    
+    const handleHeadcountChange = event => {
+        const value = event.target.value
+        setCount(value)
+        props.headcount(props.bill,value)
+    }
+    
+    const toggleRoundUp = () => {
+        setRoundUp(roundUp => !roundUp)  
+        props.rounding(!roundUp)
+    }
+
+    return(
+        <>        
+        <div className='bill'>
+            <div className="people">
+                    <input className="" type="number" min="1" min-step="1" placeholder="1" value={count} onChange={handleHeadcountChange} />
+                    <p>{count>1 ? 'people' : 'person'}</p>
+            </div>
+                <div className='bill-display'>
+                    {toggle ?        
+                        <div className='total'>
+                                <H1 className='tip' value={props.tip}>${props.tip} tip</H1>
+                                <H1 className='final' value={props.bill_tip}>${props.bill_tip}</H1>
+                        </div>
+                        :
+                        <div className='each'>
+                            <H1 className='tip' value={props.tip_share}>${props.tip_share} tip</H1>
+                            <H1 className='final' value={props.share}>${props.share}</H1>
+                        </div>            
+                    }
+                    <Toggle toggle={toggle} className={props.count>1 ? "toggle"  : "toggle disabled"} onClick={()=>setToggle(toggle => !toggle)}>{toggle ? 'Total' : 'Each'}</Toggle>
+                </div>
+                <div className='round small-container'>
+                    <button className={roundUp ? 'active nearest' : 'nearest'} onClick={(toggleRoundUp)}>Nearest $</button>
+            </div>
+        </div>   
+        </>
+    );
+    }
+
+export default Bill
